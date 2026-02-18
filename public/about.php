8c8d04b852712bdb9a6c6a091c740ebb04fb0c00
Andras Sagi <andras@pagem.hu>
2026-02-18 20:22:21 +0100

    about

--- public/about.php
+++ public/about.php
@@ -9,12 +9,17 @@ include __DIR__ . '/../includes/header.php';
 <section class="section">
     <div class="container">
         <h1 class="section-title">Rólunk - <?php echo e($company['name']); ?></h1>
-        
         <div style="max-width: 800px; margin: 0 auto;">
             <p>Cégünk 2019 óta foglalkozik egyedi bútorok tervezésével és gyártásával, azonban a személyes, szakma iránti elköteleződésem ennél jóval régebbre nyúlik vissza. Több mint 30 éve dolgozom az egyedi bútorok világában, ahol a minőség, a precizitás és az ügyfél elképzeléseinek maradéktalan megvalósítása mindig elsődleges szempont volt. Minden általunk készített bútor egyedi tervezés eredménye, amely a funkcionalitást és az esztétikát ötvözi, legyen szó konyháról, nappaliról, hálószobáról vagy teljes belsőépítészeti megoldásról.</p>
             <p>Célunk, hogy időtálló, személyre szabott bútorokat alkossunk, amelyek hosszú távon is értéket képviselnek. Hiszünk abban, hogy az igazi minőség a részletekben rejlik. Gondos tervezés, válogatott alapanyagok és precíz kivitelezés – ezek mentén készülnek bútoraink.</p>
-
-            <h2>Szolgáltatási területünk</h2>
+        </div>
+    </div>
+</section>
+<section class="section separator"></section>
+<section class="section">
+    <div class="container">
+        <h1 class="section-title">Szolgáltatási területünk</h1>
+        <div style="max-width: 800px; margin: 0 auto;">
             <p>Büszkén szolgáljuk ügyfeleinket <?php echo e($company['region']); ?> területén. Műhelyünk <?php echo e($company['address']['city']); ?>en található. </p>
             
             <p>Ha az elsődleges szolgáltatási területünkön kívül van, kérjük, <a href="/contact.php">lépjen velünk kapcsolatba</a> a projekt megbeszéléséhez—lehet, hogy képesek vagyunk kielégíteni az Ön igényeit.</p>
@@ -26,5 +31,4 @@ include __DIR__ . '/../includes/header.php';
         </div>
     </div>
 </section>
-
 <?php include __DIR__ . '/../includes/footer.php'; ?>
